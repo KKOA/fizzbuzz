00fdcf44a01068f535601ac58be22a338a5643f4
keith(Toshiba - mac) <ka33145@googlemail.com>
2017-08-28 12:31:08 +0100

    Told fizzbuzz_spec where to find the fizzbuzz method

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,3 +1,8 @@
+#require 'fizzbuzz' #Tell file where find fizzbuzz method
+#RSpec actually adds lib to its LOAD_PATH by default.rb
+# extension 'rb' is optional as Ruby will infer the .rb extension if ommitted
+require_relative '../lib/fizzbuzz'
+
 describe 'fizzbuzz' do
   it 'returns fizz when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
